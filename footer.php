3b66cfdd8b08f2dec6b209d7bdb6e696f0224810
DESKTOP-6PKSSOD\Studio <thank@grasp.asia>
2020-03-24 18:50:55 +0700

    delete

--- footer.php
+++
@@ -1,8 +0,0 @@
-   <footer>
-      <div class="text-center p-5">
-         <img src="<?php echo "assets/images/ais-logo.png" ?>" alt="" style="max-width:150px;">
-      </div>
-      <a href="index.php" class="home-back">กลับสู่หน้าหลัก</a>
-   </footer>
-</body>
-</html>
